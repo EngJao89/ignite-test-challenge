21d4790e6b93bbd3ecf98274ae1237f1f366fcab
EngJao89 <eng_joao@outlook.com>
2023-06-28 16:13:49 -0300

    feat(desafio-testes-ignite): implementa desafios do módulo de testes do ignite nodejs

---
+++ src/database/migrations/1687974162155-AlterUserAddTransfers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterUserAddTransfers1687974162155 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
